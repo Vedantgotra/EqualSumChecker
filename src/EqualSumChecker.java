5ff6143bd3970263839a366b1b3a6577683797a4
Vedantgotra <88156427+Vedantgotra@users.noreply.github.com>
2022-03-17 13:20:27 +0530

    Initial commit

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+        public static boolean hasEqualSum(int a,int b,int c){
+            return a + b == c;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1,2,3));
+
+    }
+}
---
+++ src/teen.java
@@ -0,0 +1,6 @@
+public class teen {
+    public static boolean hasTeen(int a,int b,int c){
+        return a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19;
+
+    }
+}
